582141bee2a9544e2a39fd5482b5d328815a09cb
SHIKAph <163802807+SHIKAph@users.noreply.github.com>
2025-12-11 05:37:19 +0000

    Feat: First step of Common.h

--- include/Common.h
+++ include/Common.h
@@ -1,1 +1,32 @@
+#pragma once
+
+#include <cmath> 
+#include <cstdint>
 #include <immintrin.h>
+
+#ifdef _MSC_VER
+    #define ALIGN16 __declspec(align(16))
+#else
+    #define ALIGN16 __attribute__((aligned(16)))
+#endif
+
+namespace SHIKA {
+
+   //Define Constant Values & Helpers
+   constexpr float PI = 3.1415926535f;
+   constexpr float TwoPI = PI * 2.0f;
+   constexpr float PiDiv2 = PI / 2.0f;
+
+   constexpr float EPSILON = 1e-5f;
+
+   inline float ToRadian(float deg) {
+        return deg * (PI / 180.0f);
+    }
+
+    inline float ToDegree(float rad) {
+        return rad * (180.0f / PI);
+    }
+
+}
+
+inline float DtR()

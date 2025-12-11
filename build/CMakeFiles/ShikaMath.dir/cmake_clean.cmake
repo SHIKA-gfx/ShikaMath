a8dde109b3ab36dfd3b62bc38b3ffb62719282ca
SHIKAph <163802807+SHIKAph@users.noreply.github.com>
2025-12-11 07:07:14 +0000

    Feat: Initialize project with SIMD-aligned Vector3 and CMake setup

---
+++ build/CMakeFiles/ShikaMath.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShikaMath.dir/src/Vector3.cpp.o"
+  "CMakeFiles/ShikaMath.dir/src/Vector3.cpp.o.d"
+  "libShikaMath.a"
+  "libShikaMath.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShikaMath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TestApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestApp.dir/tests/MathTest.cpp.o"
+  "CMakeFiles/TestApp.dir/tests/MathTest.cpp.o.d"
+  "TestApp"
+  "TestApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
